f2a61328fc3e9912e2604a3853342878b5466648
sreelakshmi665 <78396216+sreelakshmi665@users.noreply.github.com>
2021-06-15 11:51:02 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,10 +11,10 @@ resource "aws_s3_bucket" "b" {
 resource "aws_s3_bucket_object" "object" {
   bucket = "srees3bucketusingterraform"
   key    = "sreesra"
-  source = "path/to/file"
+  source = "C:/Users/vscr9/.m2/repository/CodingChallenge-0.0.1-SNAPSHOT-19.jar"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("path/to/file")
+  etag = filemd5("C:/Users/vscr9/.m2/repository/CodingChallenge-0.0.1-SNAPSHOT-19.jar")
 }
